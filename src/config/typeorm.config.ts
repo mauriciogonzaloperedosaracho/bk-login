05915b0eb478d9e78029ff4a47320e5d01c31ad9
Mauricio Gonzalo Peredo Saracho <mauriciogonzaloperedo28@gmail.com>
2022-10-10 08:58:23 -0400

    first commit

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
-import { Causalesion } from 'src/SP/entities/causalesion.entity';
-import { Equipojugador } from 'src/SP/entities/equipojugador.entity';
-import { Gravedadlesion } from 'src/SP/entities/gravedadlesion.entity';
-import { Jugador } from 'src/SP/entities/jugador.entity';
-import { Lesion } from 'src/SP/entities/lesion.entity';
-import { Localizacionlesion } from 'src/SP/entities/localizacionlesion.entity';
-import { Momentolesion } from 'src/SP/entities/momentolesion.entity';
-import { Posiblelesion } from 'src/SP/entities/posiblelesion.entity';
-import { Posicionjugador } from 'src/SP/entities/posicionjugador.entity';
-import { Reportarlesion } from 'src/SP/entities/reportarlesion.entity';
-import { Tratamientolesion } from 'src/SP/entities/tratamientolesion.entity';
+// import { Causalesion } from 'src/SP/entities/causalesion.entity';
+// import { Equipojugador } from 'src/SP/entities/equipojugador.entity';
+// import { Gravedadlesion } from 'src/SP/entities/gravedadlesion.entity';
+// import { Jugador } from 'src/SP/entities/jugador.entity';
+// import { Lesion } from 'src/SP/entities/lesion.entity';
+// import { Localizacionlesion } from 'src/SP/entities/localizacionlesion.entity';
+// import { Momentolesion } from 'src/SP/entities/momentolesion.entity';
+// import { Posiblelesion } from 'src/SP/entities/posiblelesion.entity';
+// import { Posicionjugador } from 'src/SP/entities/posicionjugador.entity';
+// import { Reportarlesion } from 'src/SP/entities/reportarlesion.entity';
+// import { Tratamientolesion } from 'src/SP/entities/tratamientolesion.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -18,9 +18,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   port: 5432,
   username: 'postgres',
   password: 'postgres',
-  database: 'specialtraining_db',
-  entities: [User, Lesion, Tratamientolesion, Gravedadlesion,
-  Posiblelesion, Reportarlesion, Jugador, Equipojugador,
-  Posicionjugador, Momentolesion, Localizacionlesion, Causalesion],
+  database: 'my_db',
+  entities: [User],
   synchronize: true,
 };
